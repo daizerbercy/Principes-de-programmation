98dc59e5c9ac9832bd44af875c0c8c44d57b261e
daizerbercy <158037392+daizerbercy@users.noreply.github.com>
2026-03-27 20:15:40 +0100

    Update stylesheet link for students view

--- PHP/views/students.php
+++ PHP/views/students.php
@@ -1,9 +1,14 @@
+<?php
+require_once __DIR__ . '/../services/StudentService.php';
+$students = StudentService::getAllStudents();
+?>
+
 <!DOCTYPE html>
 <html lang="fr"> 
 <head> 
     <meta charset="UTF-8"> 
     <title>Liste des etudiants</title> 
-    <link rel="stylesheet" href = "./assets/style.css"> 
+    <link rel="stylesheet" href = "/student_client/assets/style.css"> 
 </head>
 <body>
     <h1>Liste des etudiants</h1> 
