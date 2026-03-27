9b723bc90d318ff13fc33eddc4ee5cc13fa1d917
daizerbercy <158037392+daizerbercy@users.noreply.github.com>
2026-03-27 20:11:50 +0100

    Add test_api4.php to display student list

---
+++ PHP/tests/test_api4.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once 'config/config.php';
+require_once 'services/StudentService.php';
+
+// Appel du service
+$students = StudentService::getAllStudents();
+
+echo "<h1>Liste des étudiants</h1>";
+
+foreach ($students as $student) {
+    echo $student['name'] . " - " . $student['age'] . " ans<br>" ; 
+}
